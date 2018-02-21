2d716b51f00186db87f63a9dd5a3208d3c5e6302
LonelyWolf1337 <Alexander.Raschl@aon.at>
2018-02-21 14:29:13 +0100

    SAR development started

--- sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/SpecifiedArrayList.java
+++ sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/SpecifiedArrayList.java
@@ -1,5 +1,224 @@
 package org.graalvm.collections.list;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * The SpecifiedArrayList is a specified version of the Java.Util.ArrayList that has been made for
+ * the graal Compiler to enhance its performance and to prevent from pollution of the users type
+ * profile.
+ * <p>
+ * It provides the most important functionalities of the original ArrayList. The API is basically a
+ * copy of the java.util.List Interface although some functionalities are left out since they are
+ * not needed in the graal compiler.
+ *
+ *
+ * @author Alex R.
+ *
+ */
 public interface SpecifiedArrayList<E> extends Iterable<E>, Cloneable {
+
+    /**
+     * Returns the current size of the list
+     *
+     * @return the number of elements in this list
+     *
+     */
+    public int size();
+
+    /**
+     * Returns true if this list contains no elements.
+     *
+     * @return true if size is equal to 0
+     */
+    public boolean isEmpty();
+
+    /**
+     * Returns false if o is null or true if at least one of the elements in this list are equal to the
+     * Object o
+     *
+     * @param o
+     * @return true if o is not null and o is in list
+     */
+    public boolean contains(Object o);
+
+    /**
+     * An Array of the Objects that are stored in this List. The Array is <tt>size<tt> elements long.
+     *
+     * @return Array of Objects stored in List
+     */
+    public Object[] toArray();
+
+    /**
+     * Adds the the Element e to the List. If the Array has reached its max capacity it will grow to
+     * hold more elements. Returns <tt>true<tt> if insertion was successful.
+     *
+     * @param e
+     * @return <tt>true<tt> if the element has been inserted successfully else <tt>false<tt>
+     */
+    public boolean add(E e);
+
+    /**
+     * Removes the first occurrence of an Object form the list if it exists otherwise returns false
+     *
+     * @return <tt>true<tt> if element has been found and removed else <tt>false<tt>
+     */
+    public boolean remove(Object o);
+
+    /**
+     * Checks if the List contains all of the elements stored in the given collection. Basically uses
+     * the contains function on each element in the collection.
+     *
+     *
+     * @param c collection of elements to be checked
+     * @return <tt>true<tt> iff all elements of c are located in the List else <tt>false<tt>
+     */
+    public boolean containsAll(Collection<?> c);
+
+    /**
+     * Adds the whole collection c to the ArrayList.
+     *
+     * @param c Collection of elements to be added to the list
+     * @return <tt>true<tt> if all elements have been added successfully else <tt>false<tt>
+     */
+    public boolean addAll(Collection<? extends E> c); // TODO Check if EnsureCapacity is useful here
+
+    /**
+     * TODO CHECK IF NEEDED
+     *
+     * @param index
+     * @param c
+     * @return
+     */
+    public boolean addAll(int index, Collection<? extends E> c);
+
+    /**
+     * Removes all the elements of c from the ArrayList.
+     *
+     * @param c Collection of elements to be removed from the list
+     * @return <tt>true<tt> iff the list is changed else <tt>false<tt>
+     */
+    public boolean removeAll(Collection<?> c);
+
+    /**
+     * Retains only the elements that are stored in the Collection c.
+     *
+     * @param c Collection of elements to be retained
+     * @return <tt>true<tt> iff the list is changed else <tt>false<tt>
+     */
+    public boolean retainAll(Collection<?> c);
+
+    /**
+     * Deletes all elements from this list.
+     */
+    public void clear();
+
+    /**
+     * Get the Element at given Index
+     *
+     * @param index of element to be retrieved
+     * @return Element at given index
+     *
+     * @throws ArrayIndexOutOfBoundsException if Index is out of range
+     */
+    public E get(int index);
+
+    /**
+     * Replaces the Element at position index with the given element e.
+     *
+     * @param index of element to be retrieved
+     * @param element to be stored in list at given index
+     * @return Element previously located at given index
+     *
+     * @throws ArrayIndexOutOfBoundsException if Index is out of range
+     */
+    public E set(int index, E element);
+
+    /**
+     * TODO CHECK IF NEEDED due to Performance
+     *
+     * @param index
+     * @param element
+     */
+    public void add(int index, E element);
+
+    /**
+     * Removes the Object at given Index from the List.
+     *
+     * @param index of Object to be removed
+     * @return removed Object
+     *
+     * @throws ArrayIndexOutOfBoundsException if Index is out of range
+     */
+    public E remove(int index);
+
+    /**
+     * Returns the Index of the first occurrence of an Object.
+     *
+     * @param o Object to search for
+     * @return Index of the first occurrence in List or -1 if it is not contained
+     */
+    public int indexOf(Object o);
+
+    /**
+     * Returns the Index of the last occurrence of an Object.
+     *
+     * @param o Object to search for
+     * @return Index of the last occurrence in List or -1 if it is not contained
+     */
+    public int lastIndexOf(Object o);
+
+    /**
+     * Generates a ListIterator over the elements in this List.
+     *
+     * @return ListIterator over the elements in this List
+     */
+    public ListIterator<E> listIterator();
+
+    /**
+     * Generates a ListIterator over the elements in this List starting at given index.
+     *
+     * @param index
+     * @return
+     * @throws IndexOutOfBoundsException if
+     */
+    public ListIterator<E> listIterator(int index);
+
+    /**
+     *
+     * @param fromIndex Index of the first Element in the new Sublist (inclusive)
+     * @param toIndex Marks the EndPoint of the Sublist (exclusive)
+     * @return A Sublist containing the Elements at fromIndex until toIndex
+     *
+     * @throws IllegalArgumentException if fromIndex > toIndex
+     * @throws IndexOutOfBoundsException if one of the given Indexes is out of Range
+     */
+    public List<E> subList(int fromIndex, int toIndex); // TODO CHECK IF NEEDED
+
+    /**
+     * Returns an Iterator over the elements in the list.
+     *
+     * @return Iterator over elements in the list
+     */
+    public Iterator<E> iterator();
+
+    /**
+     * Ensures that the internal Array is at least of size capacity. Used for adding large amounts of
+     * data effectively.
+     *
+     * @param capacity that the List should be able to hold at minimum
+     * @return
+     */
+    public boolean ensureCapacity(int capacity);
+
+    /**
+     * Ensures that the internal Array is at most of size capacity.
+     *
+     * @param capacity Capacity the list is trimmed to
+     */
+    public void trimToSize(int capacity);
 
 }
--- sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/SpecifiedArrayListImpl.java
+++ sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/SpecifiedArrayListImpl.java
@@ -6,10 +6,16 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 public final class SpecifiedArrayListImpl<E> implements SpecifiedArrayList<E> {
 
-    final static int INITIAL_CAPACITY = 16;
-    final static int GROW_FACTOR = 2;
+    // TODO CHECK if NULL Insertion and NULL Removal is needed.
+
+    private final static int INITIAL_CAPACITY = 16;
+    private final static int GROW_FACTOR = 2;
+
+    // private final static Object[] EMPTY_ARRAY = {};
 
     private int size;
     private Object elems[];
@@ -20,8 +26,12 @@ public final class SpecifiedArrayListImpl<E> implements SpecifiedArrayList<E> {
      * @param initialCapacity
      */
     protected SpecifiedArrayListImpl(int initialCapacity) {
-        this.size = 0;
-        this.elems = new Object[initialCapacity];
+        if (size >= 0) {
+            this.size = 0;
+            this.elems = new Object[initialCapacity];
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     /**
@@ -42,12 +52,6 @@ public final class SpecifiedArrayListImpl<E> implements SpecifiedArrayList<E> {
         this.elems = Arrays.copyOf(collection.toArray(), collection.size());
     }
 
-    /**
-     * Returns the current size of the list
-     *
-     * @return the number of elements in this list
-     *
-     */
     public int size() {
         return size;
     }
@@ -56,132 +60,306 @@ public final class SpecifiedArrayListImpl<E> implements SpecifiedArrayList<E> {
         return size == 0;
     }
 
-    /**
-     * Returns false if o == null or true if any of the elements in this list are equal to the Object o
-     *
-     * @param o
-     * @return true if o is not Null and o is in list
-     */
     public boolean contains(Object o) {
-        if (o == null)
-            return false;
-
-        for (Object e : elems) { // TODO REMOVE possible code duplicate with indexOf
-            if (e.equals(o))
-                return true;
-        }
-        return false;
+        return indexOf(o) >= 0;
     }
 
-    /**
-     *
-     * @return
-     */
     public Object[] toArray() {
         return Arrays.copyOfRange(elems, 0, size);
 
     }
 
     public boolean add(E e) {
-        // TODO Auto-generated method stub
-        return false;
+        if (size == elems.length)
+            grow();
+        elems[size++] = e;
+        return true;
     }
 
     /**
      * Increases the arraySize by multiplying the array length by the current GROW_FACTOR
      */
     private void grow() {
+        int newLength = elems.length * GROW_FACTOR;
+        elems = Arrays.copyOf(elems, newLength);
+    }
 
+    public boolean remove(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elems[i] == null) {
+                    fastRemove(i);
+                    return true;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elems[i])) {
+                    fastRemove(i);
+                    return true;
+                }
+
+            }
+        }
+        return false;
     }
 
     /**
-     * Removes an Object form the list if it exists otherwise returns false
+     * Removes the Object at given index without any checks.
      *
-     * @return True if element has been found and removed else false
+     * @param index index of object to be removed
      */
-    public boolean remove(Object o) {
-        // TODO Auto-generated method stub
-        return false;
+    private void fastRemove(int index) {
+        System.arraycopy(elems, index + 1, elems, index, size - index - 1);
+        elems[size--] = null;
     }
 
     public boolean containsAll(Collection<?> c) {
-        // TODO Auto-generated method stub
-        return false;
+        for (Object obj : c) {
+            if (!contains(obj))
+                return false;
+        }
+        return true;
     }
 
     public boolean addAll(Collection<? extends E> c) {
-        // TODO Auto-generated method stub
-        return false;
+        int cSize = c.size();
+        if (cSize == 0)
+            return false;
+
+        ensureCapacity(size + cSize);
+        System.arraycopy(c.toArray(), 0, elems, size, cSize);
+        size = size + cSize;
+        return true;
     }
 
+    // TODO CHECK IF NEEDED
     public boolean addAll(int index, Collection<? extends E> c) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public boolean removeAll(Collection<?> c) {
-        // TODO Auto-generated method stub
-        return false;
+        int removed = 0;
+
+        for (Object obj : c) {
+            if (remove(obj))
+                removed++;
+        }
+        if (removed >= elems.length / GROW_FACTOR + 1) {
+            trimToSize(elems.length / GROW_FACTOR + 1);
+        }
+        return removed != 0;
     }
 
     public boolean retainAll(Collection<?> c) {
-        // TODO Auto-generated method stub
-        return false;
+        int removed = 0;
+        for (int i = 0; i < size; i++) {
+            if (!c.contains(elems[i])) {
+                fastRemove(i);
+                removed++;
+            }
+        }
+        if (removed >= elems.length / GROW_FACTOR + 1) {
+            trimToSize(elems.length / GROW_FACTOR + 1);
+        }
+        return removed != 0;
     }
 
     public void clear() {
-        // TODO Auto-generated method stub
-
+        for (int i = 0; i < size; i++) {
+            elems[i] = null;
+        }
+        trimToSize(INITIAL_CAPACITY); // TODO CHECK IF SIZE shrinking is useful;
+        System.gc();
     }
 
     public E get(int index) {
-        // TODO Auto-generated method stub
-        return null;
+        checkBoundaries(index);
+        return castUnchecked(elems[index]);
     }
 
     public E set(int index, E element) {
-        // TODO Auto-generated method stub
-        return null;
+        checkBoundaries(index);
+        Object oldElem = elems[index];
+        elems[index] = element;
+        return castUnchecked(oldElem);
     }
 
     public void add(int index, E element) {
-        // TODO Auto-generated method stub
-
+        checkBoundaries(index);
+        ensureCapacity(size + 1);
+        System.arraycopy(elems, index, elems, index + 1, size - index);
+        elems[index] = element;
+        size++;
     }
 
     public E remove(int index) {
-        // TODO Auto-generated method stub
-        return null;
+        checkBoundaries(index);
+        Object oldElem = elems[index];
+        System.arraycopy(elems, index + 1, elems, index, size - index + 1);
+        size--;
+        return castUnchecked(oldElem);
     }
 
     public int indexOf(Object o) {
-        // TODO Auto-generated method stub
-        return 0;
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elems[i] == null)
+                    return i;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(elems[i]))
+                    return i;
+            }
+        }
+        return -1;
     }
 
     public int lastIndexOf(Object o) {
-        // TODO Auto-generated method stub
-        return 0;
+        if (o == null) {
+            for (int i = size - 1; i >= 0; i--) {
+                if (elems[i] == null)
+                    return i;
+            }
+        } else {
+            for (int i = size - 1; i >= 0; i--) {
+                if (o.equals(elems[i]))
+                    return i;
+            }
+        }
+        return -1;
     }
 
     public ListIterator<E> listIterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return new ListItr(0);
     }
 
     public ListIterator<E> listIterator(int index) {
-        // TODO Auto-generated method stub
-        return null;
+        return new ListItr(index);
     }
 
+    private class ListItr extends Itr implements ListIterator<E> {
+
+        ListItr(int startIndex) {
+            super();
+            checkBoundaries(startIndex);
+            super.cursor = startIndex;
+        }
+
+        public boolean hasPrevious() {
+            return super.cursor >= 0;
+        }
+
+        public E previous() {
+
+        }
+
+        public int nextIndex() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public int previousIndex() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public void remove() {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void set(E e) {
+            // TODO Auto-generated method stub
+
+        }
+
+        public void add(E e) {
+            // TODO Auto-generated method stub
+
+        }
+
+    }
+
+    public Iterator<E> iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<E> {
+
+        protected int cursor;
+
+        Itr() {
+            cursor = 0;
+        }
+
+        public boolean hasNext() {
+            return cursor < size;
+        }
+
+        public E next() {
+            Object elem = elems[cursor];
+            cursor++;
+            return castUnchecked(elem);
+        }
+
+    }
+
+    // TODO CHECK IF NEEDED
     public List<E> subList(int fromIndex, int toIndex) {
         // TODO Auto-generated method stub
         return null;
     }
 
-    public Iterator<E> iterator() {
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(elems);
+        result = prime * result + size;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        SpecifiedArrayListImpl<E> other = (SpecifiedArrayListImpl<E>) obj;
+        if (!Arrays.equals(elems, other.elems))
+            return false;
+        if (size != other.size)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "SpecifiedArrayListImpl [size=" + size + ", elems=" + Arrays.toString(elems) + "]";
+    }
+
+    public boolean ensureCapacity(int capacity) {
         // TODO Auto-generated method stub
-        return null;
+        return false;
+    }
+
+    public void trimToSize(int capacity) {
+        // TODO Auto-generated method stub
+    }
+
+    private void checkBoundaries(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private E castUnchecked(Object obj) {
+        return (E) obj;
     }
 
 }
