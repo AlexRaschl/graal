10cde846e975d4010bd0af0f27604d8e35674e78
AlexRaschl <Alexander.Raschl@aon.at>
2018-08-12 12:23:40 +0200

    Test 20 itr

--- sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/SpecifiedArrayList.java
+++ sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/SpecifiedArrayList.java
@@ -79,37 +79,37 @@ public class SpecifiedArrayList<E> extends AbstractList<E> implements List<E>, R
 // return new StatisticalArrayListClone<>(initalCapacity);
 // }
 
-    public static <E> SpecifiedArrayList<E> createNew() {
-        return new StatisticalSpecifiedArrayListImpl<>();
-    }
-
-    public static <E> SpecifiedArrayList<E> createNew(final int initialCapacity) {
-        return new StatisticalSpecifiedArrayListImpl<>(initialCapacity);
-    }
-
-    public static <E> SpecifiedArrayList<E> createNew(Collection<E> c) {
-        return new StatisticalSpecifiedArrayListImpl<>(c);
-    }
-
-    public static <E> SpecifiedArrayList<E> createNewFixed(final int initialCapacity) {
-        return new StatisticalSpecifiedArrayListImpl<>(initialCapacity);
-    }
-//
 // public static <E> SpecifiedArrayList<E> createNew() {
-// return new SpecifiedArrayList<>();
+// return new StatisticalSpecifiedArrayListImpl<>();
 // }
 //
 // public static <E> SpecifiedArrayList<E> createNew(final int initialCapacity) {
-// return new SpecifiedArrayList<>(initialCapacity);
+// return new StatisticalSpecifiedArrayListImpl<>(initialCapacity);
 // }
 //
 // public static <E> SpecifiedArrayList<E> createNew(Collection<E> c) {
-// return new SpecifiedArrayList<>(c);
+// return new StatisticalSpecifiedArrayListImpl<>(c);
 // }
 //
 // public static <E> SpecifiedArrayList<E> createNewFixed(final int initialCapacity) {
-// return new FixedCapacitiySpecifiedArrayList<>(initialCapacity);
+// return new StatisticalSpecifiedArrayListImpl<>(initialCapacity);
 // }
+
+    public static <E> SpecifiedArrayList<E> createNew() {
+        return new SpecifiedArrayList<>();
+    }
+
+    public static <E> SpecifiedArrayList<E> createNew(final int initialCapacity) {
+        return new SpecifiedArrayList<>(initialCapacity);
+    }
+
+    public static <E> SpecifiedArrayList<E> createNew(Collection<E> c) {
+        return new SpecifiedArrayList<>(c);
+    }
+
+    public static <E> SpecifiedArrayList<E> createNewFixed(final int initialCapacity) {
+        return new FixedCapacitiySpecifiedArrayList<>(initialCapacity);
+    }
 
     public static SimpleIntSpecifiedArrayList createNewIntList(final int initialCapacity) {
         return new SimpleIntSpecifiedArrayList(initialCapacity);
@@ -404,10 +404,10 @@ public class SpecifiedArrayList<E> extends AbstractList<E> implements List<E>, R
     /**
      * Copied 1:1 from ArrayList
      *
-     * Removes from this list all of the elements whose index is between {@code fromIndex}, inclusive,
-     * and {@code toIndex}, exclusive. Shifts any succeeding elements to the left (reduces their index).
-     * This call shortens the list by {@code (toIndex - fromIndex)} elements. (If
-     * {@code toIndex==fromIndex}, this operation has no effect.)
+     * Removes from this list all of the elements whose index is between {@code fromIndex},
+     * inclusive, and {@code toIndex}, exclusive. Shifts any succeeding elements to the left
+     * (reduces their index). This call shortens the list by {@code (toIndex - fromIndex)} elements.
+     * (If {@code toIndex==fromIndex}, this operation has no effect.)
      *
      * @throws IndexOutOfBoundsException if {@code fromIndex} or {@code toIndex} is out of range
      *             ({@code fromIndex < 0 ||
@@ -905,28 +905,29 @@ public class SpecifiedArrayList<E> extends AbstractList<E> implements List<E>, R
     /**
      * Returns a view of theensureCapacityInternalAl portion of this list between the specified
      * {@code fromIndex}, inclusive, and {@code toIndex}, exclusive. (If {@code fromIndex} and
-     * {@code toIndex} are equal, the returned list is empty.) The returned list is backed by this list,
-     * so non-structural changes in the returned list are reflected in this list, and vice-versa. The
-     * returned list supports all of the optional list operations.
+     * {@code toIndex} are equal, the returned list is empty.) The returned list is backed by this
+     * list, so non-structural changes in the returned list are reflected in this list, and
+     * vice-versa. The returned list supports all of the optional list operations.
      *
      * <p>
-     * This method eliminates the need for explicit range operations (of the sort that commonly exist
-     * for arrays). Any operation that expects a list can be used as a range operation by passing a
-     * subList view instead of a whole list. For example, the following idiom removes a range of
-     * elements from a list:
+     * This method eliminates the need for explicit range operations (of the sort that commonly
+     * exist for arrays). Any operation that expects a list can be used as a range operation by
+     * passing a subList view instead of a whole list. For example, the following idiom removes a
+     * range of elements from a list:
      *
      * <pre>
      * list.subList(from, to).clear();
      * </pre>
      *
-     * Similar idioms may be constructed for {@link #indexOf(Object)} and {@link #lastIndexOf(Object)},
-     * and all of the algorithms in the {@link Collections} class can be applied to a subList.
+     * Similar idioms may be constructed for {@link #indexOf(Object)} and
+     * {@link #lastIndexOf(Object)}, and all of the algorithms in the {@link Collections} class can
+     * be applied to a subList.
      *
      * <p>
      * The semantics of the list returned by this method become undefined if the backing list (i.e.,
      * this list) is <i>structurally modified</i> in any way other than via the returned list.
-     * (Structural modifications are those that change the size of this list, or otherwise perturb it in
-     * such a fashion that iterations in progress may yield incorrect results.)
+     * (Structural modifications are those that change the size of this list, or otherwise perturb
+     * it in such a fashion that iterations in progress may yield incorrect results.)
      *
      * @throws IndexOutOfBoundsException {@inheritDoc}
      * @throws IllegalArgumentException {@inheritDoc}
