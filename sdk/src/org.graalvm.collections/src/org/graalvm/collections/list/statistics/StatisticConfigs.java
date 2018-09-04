30e6a55b2b64c893a0cad59eaa90526411b863af
LonelyWolf1337 <Alexander.Raschl@aon.at>
2018-09-04 18:29:20 +0200

    Disabled tracking

--- sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/statistics/StatisticConfigs.java
+++ sdk/src/org.graalvm.collections/src/org/graalvm/collections/list/statistics/StatisticConfigs.java
@@ -33,7 +33,7 @@ public class StatisticConfigs {
     public static final boolean AGGREGATE_SAME_CLASSES = false;
 
     // Enable Tracking
-    public static final boolean TRACKING_ENABLED = true;
+    public static final boolean TRACKING_ENABLED = false;
 
     // Track all Allocation Sites or only the Sites listed in trackedSites HashSet
     public static final boolean TRACKS_ALL = true;
